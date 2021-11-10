379d86614ec1236c58f91ae978381815bcf5c342
PauloH02 <92831284+PauloH02@users.noreply.github.com>
2021-11-09 21:30:20 -0300

    Add files via upload

--- Operações.java
+++ Operações.java
@@ -1,8 +1,8 @@
 package Calculadora;
 
 public class Operações {
-    private final float numero1;
-    private final float numero2;
+    private float numero1;
+    private float numero2;
 public Operações(float numero1, float numero2){
     this.numero1=numero1;
     this.numero2=numero2;
