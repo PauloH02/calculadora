04d38cadc5954a1e12cf16001b9a514c62052c16
PauloH02 <92831284+PauloH02@users.noreply.github.com>
2021-11-09 21:27:20 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Informe um Número: ");
-        float v1=scan.nextFloat();
+        float v1 = scan.nextFloat();
         System.out.println("Informe o sinal da operação: ");
         scan.nextLine();
-        String sinal=scan.nextLine();
+        String sinal = scan.nextLine();
         System.out.println("Informe outro Número: ");
-        float v2=scan.nextFloat();
-Operações oper=new Operações(v1, v2);
+        float v2 = scan.nextFloat();
+Operações oper = new Operações(v1, v2);
         System.out.println("Resultado é: ");
 switch(sinal){
     case "+":
